69200c2f52a431cd412bedb9fea7fe5403d287f5
gtumedei <giannitumedei@gmail.com>
2025-10-01 12:44:31 +0100

    Improve error handling when running shell commands

--- src/shared/command.ts
+++ src/shared/command.ts
@@ -1,11 +1,22 @@
 export const executeCommand = async (command: string, ...args: string[]) => {
   const cmd = new Deno.Command(command, { args: args })
-  const { success, code, stdout, stderr } = await cmd.output()
+  const { success, code, stdout: stdoutBuff, stderr: stderrBuff } = await cmd.output()
   const decoder = new TextDecoder()
-  return {
-    success,
-    code,
-    stdout: decoder.decode(stdout),
-    stderr: decoder.decode(stderr),
+  const stdout = decoder.decode(stdoutBuff)
+  const stderr = decoder.decode(stderrBuff)
+  if (!success) throw new CommandError({ code, stdout, stderr })
+  return { stdout, stderr }
+}
+
+export class CommandError extends Error {
+  code: number
+  stdout?: string
+  stderr?: string
+
+  constructor(params: { code: number; stdout?: string; stderr?: string }) {
+    super("Command completed with errors")
+    this.code = params.code
+    this.stdout = params.stdout
+    this.stderr = params.stderr
   }
 }
