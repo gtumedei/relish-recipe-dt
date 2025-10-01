7f8c7722c931d272375f9ff6eeabb3012ca32326
gtumedei <giannitumedei@gmail.com>
2025-10-01 10:59:46 +0100

    Implement a simple ingestion pipeline

--- src/ingestor/main.ts
+++ src/ingestor/main.ts
@@ -1,11 +1,15 @@
+import "@relish/env"
+
 import { logger } from "@relish/shared"
+import { youtube } from "@relish/source-adapters"
+import { join } from "@std/path"
+import { ensureDir } from "@std/fs"
 
-logger.i("Hello, World!")
+const tmpDir = join(import.meta.dirname!, "..", "..", "tmp")
+ensureDir(tmpDir)
 
-import { db } from "@relish/database"
-const dbRes = await db.dinosaur.findMany()
-logger.i(dbRes)
+const ytRes = await youtube.fetch()
 
-// import { youtube } from "@relish/source-adapters"
-// const ytRes = await youtube.fetch()
-// logger.i(ytRes)
+const filename = join(tmpDir, "res-youtube.json")
+await Deno.writeTextFile(filename, JSON.stringify(ytRes, null, 2))
+logger.i(`Results saved to ${filename}`)
