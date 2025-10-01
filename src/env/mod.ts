8523e7a3aa1d7cd3ec41e2fe76dcabd490dbd06e
gtumedei <giannitumedei@gmail.com>
2025-10-01 10:57:58 +0100

    Load env vars to Deno.env

--- src/env/mod.ts
+++ src/env/mod.ts
@@ -5,9 +5,10 @@ import { z } from "zod"
 const EnvSchema = z.object({
   DATABASE_URL: z.string().min(1),
   YOUTUBE_API_KEY: z.string().min(1),
+  GOOGLE_GENERATIVE_AI_API_KEY: z.string().min(1),
 })
 
-const rawEnv = await load({ envPath: join(import.meta.dirname ?? "./", ".env") })
+const rawEnv = await load({ envPath: join(import.meta.dirname ?? "./", ".env"), export: true })
 const parsedEnv = EnvSchema.safeParse(rawEnv)
 
 if (parsedEnv.success === false) {
