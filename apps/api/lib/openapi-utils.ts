067056a307b2d979181322d3f42f9dfc5621593d
gtumedei <giannitumedei@gmail.com>
2026-02-13 10:25:31 +0100

    Initial Hono server setup

---
+++ apps/api/lib/openapi-utils.ts
@@ -0,0 +1,39 @@
+import { resolver } from "hono-openapi"
+import z, { ZodType } from "zod"
+
+export const security = {
+  none: [{}],
+  basicAuth: [{ basicAuth: [] }],
+}
+
+export const text = (params: { description: string }) => ({
+  description: params.description,
+  content: { "text/plain": { schema: resolver(z.string()) } },
+})
+
+export const json = (params: { description: string; schema: ZodType }) => ({
+  description: params.description,
+  content: {
+    "application/json": { schema: resolver(params.schema) },
+  },
+})
+
+const PropertyKeySchema = z.union([z.string(), z.number()])
+
+export const ValidatorErrorSchema = z.object({
+  data: z.any(),
+  error: z.array(
+    z.object({
+      message: z.string(),
+      path: z.array(z.union([PropertyKeySchema, z.object({ key: PropertyKeySchema })])).optional(),
+    }),
+  ),
+})
+export type ValidationError = z.infer<typeof ValidatorErrorSchema>
+
+export const validationError = {
+  description: "Validation error",
+  content: {
+    "application/json": { schema: resolver(ValidatorErrorSchema) },
+  },
+}
--- apps/api/main.ts
+++ apps/api/main.ts
@@ -1,9 +1,67 @@
+import { Scalar } from "@scalar/hono-api-reference"
+import { blue } from "@std/fmt/colors"
 import { Hono } from "hono"
+import { describeRoute, openAPIRouteHandler } from "hono-openapi"
+import { cors } from "hono/cors"
+import { serveStatic } from "hono/deno"
+import { logger } from "hono/logger"
+import { text } from "~/lib/openapi-utils.ts"
 
 const app = new Hono()
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!")
-})
+app.use(logger())
+app.use(cors())
 
-Deno.serve(app.fetch)
+app.get(
+  "/",
+  describeRoute({
+    tags: ["Home"],
+    responses: {
+      200: text({ description: "Hello world route" }),
+    },
+  }),
+  (c) => {
+    return c.text("Hello Hono!")
+  },
+)
+
+app.get(
+  "/docs",
+  openAPIRouteHandler(app, {
+    documentation: {
+      info: {
+        title: "RELISH Recipe Digital Twin",
+        version: "0.1.0",
+        description: "API server for  the RELISH Recipe Digital Twin.",
+      },
+      /* components: {
+        securitySchemes: {
+          basicAuth: { type: "http", scheme: "basic" },
+        },
+      },
+      security: security.basicAuth, */
+    },
+  }),
+)
+app.get(
+  "/scalar",
+  Scalar({
+    url: "/docs",
+    favicon: "/favicon.webp",
+    agent: { disabled: true },
+    showDeveloperTools: "never",
+  }),
+)
+
+app.use("/favicon.webp", serveStatic({ path: "./public/favicon.webp" }))
+
+app.notFound((c) => c.json({ message: "Not found" }, 404))
+
+const msg = `
+🍛 Relish server started
+
+   API:   ${blue("http://localhost:8000/api")}
+   Docs:  ${blue("http://localhost:8000/scalar")}
+`
+
+Deno.serve({ onListen: () => console.log(msg) }, app.fetch)
