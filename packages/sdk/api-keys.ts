dfe6c3660eae5748428ddeb6b68217c53cb0bd9c
gtumedei <giannitumedei@gmail.com>
2026-02-13 14:19:24 +0100

    Implement API keys SDK

--- packages/sdk/api-keys.ts
+++ packages/sdk/api-keys.ts
@@ -1,28 +1,50 @@
-import { db, type ApiKey } from "@relish/storage"
+import { db, type $Enums, type ApiKey } from "@relish/storage"
+import { nanoid } from "nanoid"
+import z from "zod"
 import { SdkError } from "~/error.ts"
+
+const CollectionAccessSchema = z.array(
+  z.object({
+    collection: z.enum(["Dish", "Recipe", "UserRecipe", "Ingredient", "Tool"]),
+    rules: z.array(
+      z.enum(["CREATE", "READ", "UPDATE", "DELETE", "TASKS"] satisfies $Enums.AccessRule[]),
+    ),
+  }),
+)
 
 export const apiKeys = {
   list: async () => {
     const keys = await db.apiKey.findMany()
     return keys
   },
-  create: async () => {},
-  get: async (params: { id: string }) => {
-    const key = await db.apiKey.findUnique({ where: { id: params.id } })
+  create: async (params: { data: Pick<ApiKey, "name" | "access"> }) => {
+    if (!CollectionAccessSchema.safeParse(params.data.access))
+      throw new SdkError({ code: "BAD_REQUEST", message: "Invalid collection access object" })
+    const key = await db.apiKey.create({
+      data: { key: nanoid(32), ...params.data },
+    })
+    return key
+  },
+  get: async (params: { key: string }) => {
+    const key = await db.apiKey.findUnique({ where: { key: params.key } })
     if (!key) throw new SdkError({ code: "NOT_FOUND" })
     return key
   },
-  update: async (params: { id: string; data: Pick<ApiKey, "name" | "access"> }) => {
-    const key = await db.apiKey.findUnique({ where: { id: params.id } })
+  update: async (params: { key: string; data: Pick<ApiKey, "name" | "access"> }) => {
+    const key = await db.apiKey.findUnique({ where: { key: params.key } })
     if (!key) throw new SdkError({ code: "NOT_FOUND" })
-    await db.apiKey.update({
-      where: { id: params.id },
+    if (!CollectionAccessSchema.safeParse(params.data.access))
+      throw new SdkError({ code: "BAD_REQUEST", message: "Invalid collection access object" })
+    const updatedKey = await db.apiKey.update({
+      where: { key: params.key },
       data: params.data,
     })
+    return updatedKey
   },
-  delete: async (params: { id: string }) => {
-    const key = await db.apiKey.findUnique({ where: { id: params.id } })
+  delete: async (params: { key: string }) => {
+    const key = await db.apiKey.findUnique({ where: { key: params.key } })
     if (!key) throw new SdkError({ code: "NOT_FOUND" })
-    await db.apiKey.delete({ where: { id: params.id } })
+    const deletedKey = await db.apiKey.delete({ where: { key: params.key } })
+    return deletedKey
   },
 }
